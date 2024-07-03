52baa582da2c83c45b82d156c7f62322c50c3ebb
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2024-07-03 12:02:37 -0500

    Adjust window assignments

--- config.h
+++ config.h
@@ -36,9 +36,11 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class             instance   title       tags mask     isfloating   monitor */
+  { "firefox",         NULL,      NULL,       1 << 1,       0,           -1 },
+	{ "gloCOM",          "gloCOM",  NULL,       1 << 7,       0,           -1 },
+	{ "TelegramDesktop", NULL,      NULL,       1 << 6,       0,           -1 },
+	{ "winbox.exe",      NULL,      NULL,       1 << 2,       0,           -1 },
 };
 
 /* layout(s) */
