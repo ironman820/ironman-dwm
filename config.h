525edce80f65986ac97271565bc6d8d3bb4208b7
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2024-07-25 11:09:15 -0500

    Label workspace 9

--- config.h
+++ config.h
@@ -27,7 +27,7 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "󱚾", "4", "5", "6", "", "󰏲", "9" };
+static const char *tags[]    = { "", "", "󱚾", "4", "5", "6", "", "󰏲", "" };
 static const char *tagsalt[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 static const int momentaryalttags = 1; /* 1 means alttags will show only when key is held down*/
 
